57984cfea61bbb664fefd7a908dbd026a3e2e857
moxianyou <1872499195@qq.com>
2022-09-11 10:17:33 +0800

    feat:我的页面完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,18 @@
+import store from '@/store'
 import axios from 'axios'
 
 const request = axios.create({
   baseURL: 'http://toutiao.itheima.net',
   timeout: 5000
 })
-
+request.interceptors.request.use(function (config) {
+  const {
+    getters: { isLogin },
+    state: { tokenObj }
+  } = store
+  if (isLogin) {
+    config.headers.Authorization = `Bearer ${tokenObj.token}`
+  }
+  return config
+})
 export default request
